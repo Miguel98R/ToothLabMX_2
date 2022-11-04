7657ddb88100d384188cfd89952d4487c3c4a1cb
mikeleganux1998 <111320389+mikeleganux1998@users.noreply.github.com>
2022-11-03 21:40:11 -0600

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://'+process.env.URL_SERVER+'/'+process.env.DB_NAME).then(() => {
 
-    console.log('Se conecto a la DB',process.env.DB_NAME)
+    console.log('Se conecto a la DB Local',process.env.DB_NAME)
 }).catch( (e) => {
     console.error('Error al conetarse a a db', e)
 
@@ -11,3 +11,21 @@ mongoose.connect('mongodb://'+process.env.URL_SERVER+'/'+process.env.DB_NAME).th
 
 
 module.exports = mongoose
+
+
+
+//////////////////////db en la nube//////////////////////////////////////
+
+// const mongoose = require('mongoose');
+
+
+// mongoose.connect(process.env.URL_SERVER_CLOUD).then(() => {
+
+//     console.log('Se conecto a la DB SERVER',process.env.DB_NAME)
+// }).catch( (e) => {
+//     console.error('Error al conetarse a a db', e)
+
+// });
+
+
+// module.exports = mongoose
