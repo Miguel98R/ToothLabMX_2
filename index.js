36c984cf211c8b46757526cd3ecc358272f20834
mikeleganux1998 <111320389+mikeleganux1998@users.noreply.github.com>
2022-12-06 09:33:04 -0600

    udpate db conecxion

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.set('view engine', 'pug')
 
-//middlewares
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 // parse application/json
@@ -29,7 +27,6 @@ app.use(bodyParser.json())
 app.use(morgan('dev'))
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
-
 
 //rutas
 app.use(require('./viewEngine/routes'))
