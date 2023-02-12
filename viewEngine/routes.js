3213abeb11a19cde8d81aaab8d97a75731d1748e
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2023-02-12 12:21:38 -0600

    update menu

--- viewEngine/routes.js
+++ viewEngine/routes.js
@@ -9,19 +9,23 @@ let menu = [
         elements: [
             {
                 icon: 'fas fa-tachometer-alt',
+                img : './public/img/iconsMenu/ordenes.png',
                 title: 'Dashboard',
                 ref: '/panel'
             }, {
                 icon: 'fas fa-teeth',
+                img : './public/img/iconsMenu/products.png',
                 title: 'Productos',
                 ref: '/products'
             }, {
                 icon: 'fas fa-tooth',
+                img : './public/img/iconsMenu/dentista.png',
                 title: 'Dentistas',
                 ref: '/dentistas'
             },
             {
                 icon: 'far fa-plus-square',
+                img : './public/img/iconsMenu/ordenes.png',
                 title: 'Generar orden',
                 ref: '/orders',
             },
@@ -36,36 +40,43 @@ let menu = [
 
             {
                 icon: 'fas fa-arrow-right',
+                img : './public/img/iconsMenu/entrante.png',
                 title: 'Entrantes',
                 ref: '/status_entrante',
             },
             {
                 icon: 'fas fa-business-time',
+                img : './public/img/iconsMenu/prueba.png',
                 title: 'A Prueba',
                 ref: '/status_prueba',
             },
             {
                 icon: 'fas fa-undo-alt',
+                img : './public/img/iconsMenu/regreso.png',
                 title: 'Regresadas',
                 ref: '/status_regresadas',
             },
             {
                 icon: 'fas fa-check-circle',
+                img : './public/img/iconsMenu/terminado.png',
                 title: 'Terminadas',
                 ref: '/status_terminadas',
             },
             {
                 icon: 'fas fa-exchange-alt',
+                img : './public/img/iconsMenu/cambios.png',
                 title: 'Con Cambios',
                 ref: '/status_cambios',
             },
             {
                 icon: 'fas fa-ban',
                 title: 'Canceladas con costos',
+                img : './public/img/iconsMenu/cancelarConCostos.png',
                 ref: '/status_CancelConCostos',
             },
             {
                 icon: 'fas fa-ban',
+                img : './public/img/iconsMenu/cancelado.png',
                 title: 'Canceladas',
                 ref: '/status_canceladas',
             },
@@ -86,6 +97,7 @@ router.get("/", async (req, res) => {
 
 router.get("/panel", async (req, res) => {
 
+    console.log("----------------------------",menu)
     res.render('panel', {
             title: 'ToothLabMX | Panel',
             menu: menu
