625540dce2e97d6f59531d94797cd40f4b1a525b
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2023-04-21 10:14:27 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
 let dbname = process.env.DB_NAME || 'ToothLabMX2'
-let dbProd = process.env.URL_SERVER || 'mongodb+srv://admin:admin%401998@toothlabmx2.bvx3g5s.mongodb.net'
-
+let dbProd = process.env.URL_SERVER || "mongodb+srv://admin:admin%401998@toothlabmx2.bvx3g5s.mongodb.net"
 
 mongoose.connect(dbProd + '/' + dbname).then(() => {
 
-    if (process.env.URL_SERVER.includes('admin')) {
+    if (dbProd.includes('admin')) {
 
-        console.log('Se conecto a la DB PRODUCION', process.env.DB_NAME)
+        console.log('Se conecto a la DB PRODUCION', dbname)
 
     } else {
-        console.log('Se conecto a la DB Local', process.env.DB_NAME)
+        console.log('Se conecto a la DB Local', dbname)
 
     }
 
